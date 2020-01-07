bf2c6cbbac8a85314630d76573beaffb6605408d
per1234 <accounts@perglass.com>
2020-01-07 02:50:55 -0800

    Fix typos in documentation (#1)

--- src/Arduino_CRC32.cpp
+++ src/Arduino_CRC32.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2020 Arduino.  All right reserved.
+  Copyright (c) 2020 Arduino.  All rights reserved.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
